78c506349b0e8856e25f8bee9ccb366e48f4087b
Yagnesh Modh <contact@thespecialcharacter.com>
2024-03-16 12:43:32 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("hello world");
+
+// 1 create repository on github/gitlab/bitbucket/gitea -> server url
+
+// code -> connect with remote server
+// git remote add origin <server url>
+
+// to check connection
+// git remote -v
+
+// if you want to push your code to remote server 
+// for first time
+// git push -u origin <branchname(main)>
